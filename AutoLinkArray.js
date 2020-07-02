82b272f2a5836aa0fd55fd04dd0a401720cac702
immortalmice <killer536697@gmail.com>
2020-07-02 12:09:29 +0800

    Feat: Implement of AutoLinkArray
    Add:
        Constructor of AutoLinkArray with some setting parameter.
        Method get will determine the timing to auto refactor.

--- AutoLinkArray.js
+++ AutoLinkArray.js
@@ -1,5 +1,17 @@
 const LinkArray = require("./LinkArray.js");
 
 module.exports = class AutoLinkArray extends LinkArray{
-
+	constructor(refactorBoundIn, refactorRatioIn){
+		super();
+		this.refactorBound = refactorBoundIn || 200;
+		this.refactorRatio = refactorRatioIn || 0.75;
+	}
+	get(index){
+		if(this.array.length >= this.refactorBound
+			&& (this.lastRefactorUpperBound <= 1 || this.lastRefactorUpperBound / this.array.length <= this.refactorRatio)){
+			this.refactor();
+			return this.array[index];
+		}
+		return super.get(index);
+	}
 }
