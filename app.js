cc5e286817978ceaa6da6caed90f5f2b175fc60d
immortalmice <killer536697@gmail.com>
2020-07-14 15:42:16 +0800

    Feat: Helper function to get format arrays

--- app.js
+++ app.js
@@ -14,9 +14,31 @@ let analyzer = new Analyzer(
 
 analyzer.runDefault();
 
-// let normalArray = RunTest.getFormatArray("Normal Array", []);
-// let autoArray = RunTest.getFormatArray("Auto Link Array", new AutoLinkArray());
+function getNormalFormatArray(){
+	return RunTest.getFormatArray("Normal Array", []);
+}
 
-// let testArray = RunTest.genTest(100000, ["GET", "PUSH", "UNSHIFT", "POP", "SHIFT"]);
-// RunTest.runTest(testArray, true, normalArray, autoArray);
-// RunTest.testCorrectness(normalArray, autoArray);
+function getAutoFormatArray(i){
+	return RunTest.getFormatArray("Auto Link Array", new AutoLinkArray(i), {
+		"GET": (array, value) => array.get(value % array.length)
+	});
+}
+
+function getDoublyFormatArray(){
+	return RunTest.getFormatArray("Doubly Linked List", new DoublyLinkedList(), {
+		"PUSH": (array, value) => array.insert(value) ? array.size : 0,
+		"UNSHIFT": (array, value) => array.insertFirst(value) ? array.size : 0,
+		"POP": (array) => {
+			let result = array.remove();
+			return result ? result.getData() : undefined;
+		},
+		"SHIFT": (array) => {
+			let result = array.removeFirst();
+			return result ? result.getData() : undefined;
+		},
+		"GET": (array, value) => {
+			let result = array.findAt(value % array.size);
+			return result === -1 ? undefined : result.getData();
+		}
+	});
+}
