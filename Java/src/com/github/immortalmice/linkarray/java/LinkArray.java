9639e1d60bf21abb46b399ccd0a78451b42029e1
immortalmice <killer536697@gmail.com>
2020-08-11 12:05:33 +0800

    Feat: Basic class structure of LinkArray

---
+++ Java/src/com/github/immortalmice/linkarray/java/LinkArray.java
@@ -0,0 +1,24 @@
+package com.github.immortalmice.linkarray.java;
+
+import java.util.ArrayList;
+
+public class LinkArray<T>{
+	protected ArrayList<LinkArrayNode<T>> array = new ArrayList<>();
+	protected LinkArrayNode<T> head = null;
+	protected LinkArrayNode<T> tail = null;
+	protected int lowerBound = 0;
+	protected int upperBound = -1;
+	protected int lastRefactorUpperBound = -1;
+
+	protected class LinkArrayNode<F>{
+		public int index;
+		public F value;
+		public LinkArrayNode<F> next = null;
+		public LinkArrayNode<F> pre = null;
+
+		public LinkArrayNode(int indexIn, F valueIn){
+			this.index = indexIn;
+			this.value = valueIn;
+		}
+	}
+}
