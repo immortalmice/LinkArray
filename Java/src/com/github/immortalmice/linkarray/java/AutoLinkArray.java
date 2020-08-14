1c2f6ee6bd1981b904725bf0f1a7a38812d469b3
immortalmice <killer536697@gmail.com>
2020-08-14 12:03:31 +0800

    Doc: Add class AutoLinkArray's docs

--- Java/src/com/github/immortalmice/linkarray/java/AutoLinkArray.java
+++ Java/src/com/github/immortalmice/linkarray/java/AutoLinkArray.java
@@ -1,7 +1,21 @@
 package com.github.immortalmice.linkarray.java;
 
-@SuppressWarnings("unchecked")
+/**
+ * An derived class of LinkArray that will auto do refactor when it think it should.
+ * 
+ * @author Immortalmice
+ *
+ * @param <T> The type to store in
+ */
 public class AutoLinkArray<T> extends LinkArray<T>{
+	
+	/**
+	 * {@inheritDoc}
+	 * <br>
+	 * Do refactor when this need to allocate new array memory.<br>
+	 * Since copy to new array is needed, it's a best timing to do refactor at the same time.<br>
+	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void allocateNewArray(int minCapacity){
 		this.refactor((LinkArrayNode<T>[]) new LinkArrayNode[minCapacity + this.reservedCapacity]);
